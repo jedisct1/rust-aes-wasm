137c444bd0fd278c7f85d77b866da91abef25db7
Frank Denis <github@pureftpd.org>
2024-05-09 23:18:16 +0200

    Update build.zig

--- wasm-libs/build.zig
+++ wasm-libs/build.zig
@@ -5,10 +5,22 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
     const lib = b.addStaticLibrary(.{
         .name = "aes",
-        .root_source_file = .{ .path = "aes.zig" },
+        .root_source_file = b.path("aes.zig"),
         .target = target,
         .optimize = optimize,
         .strip = true,
     });
     b.installArtifact(lib);
+
+    const exe = b.addExecutable(.{
+        .name = "aes",
+        .root_source_file = b.path("aes.zig"),
+        .target = target,
+        .optimize = optimize,
+        .strip = true,
+    });
+    exe.rdynamic = true;
+    exe.wasi_exec_model = .reactor;
+    exe.entry = .disabled;
+    b.installArtifact(exe);
 }
