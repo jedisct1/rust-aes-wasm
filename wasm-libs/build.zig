eac5828cc09e53a0a3fb480003c8512b6352e7b1
Frank Denis <github@pureftpd.org>
2024-08-30 16:10:23 +0200

    Import the CBC module

--- wasm-libs/build.zig
+++ wasm-libs/build.zig
@@ -3,6 +3,12 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = .wasm32, .os_tag = .wasi } });
     const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
+
+    const cbc = b.dependency("cbc", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "aes",
         .root_source_file = b.path("aes.zig"),
@@ -10,6 +16,8 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .strip = true,
     });
+    lib.root_module.addImport("cbc", cbc.module("cbc"));
+    lib.linkLibrary(cbc.artifact("cbc"));
     b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
